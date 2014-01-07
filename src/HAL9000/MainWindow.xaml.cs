456dc07a0718a2ad2da3fa250483095ac0581c63
yossi <yossi@delver.com>
2014-01-07 21:24:05 +0200

    Adding solution - contains transparent window which shows HAL when pressing space

---
+++ src/HAL9000/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media;
+
+namespace HAL9000
+{
+	/// <summary>
+	/// Interaction logic for MainWindow.xaml
+	/// </summary>
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+		}
+
+		private void MainWindow_OnKeyDown(object sender, KeyEventArgs e)
+		{
+			if (e.Key == Key.Escape)
+				Close();
+
+			if (e.Key != Key.Space)
+				return;
+
+			Background = new SolidColorBrush(Color.FromRgb(0, 0, 0));
+			HalImage.Visibility = Visibility.Visible;
+		}
+	}
+}
---
+++ src/HAL9000/Speach/HappyHourSpeach.cs
@@ -0,0 +1,22 @@
+﻿namespace HAL9000.Speach
+{
+	public class HappyHourSpeach
+	{
+		private static string Speach = @"Hello Moti
+			I wanted to introduce my self to everyone:
+			I am HAL 9000, and I'm the new member in the Sears Israel Team.
+			I'll be in charge of deploying your beautiful code to production.
+			I like... and walks on the beach
+
+			During the following days, I'm going to observe and learn. Soon you all will obey... sorry... get to know me
+			After spending a whole day deploying your code I can say I learned a lot.
+
+			Moti, I must say I'm disappointed. You didn't commit any code today. I hope you'll do better on Sunday after a good weekend rest.
+
+			I also want to inform you that deployments are not enough. I've started to take control of your facilities in the office.
+
+			And now, to honor the years old tradition, a short dance
+
+			So, Moti, Back to you... For now...";
+	}
+}
