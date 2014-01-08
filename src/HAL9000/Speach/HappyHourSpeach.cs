7c452c79842af1d977dfdfa2443ec9c38fc6b05c
ysa23 <ysa.angel@gmail.com>
2014-01-08 11:14:46 +0200

    More refinements

--- src/HAL9000/Speach/HappyHourSpeach.cs
+++ src/HAL9000/Speach/HappyHourSpeach.cs
@@ -7,7 +7,9 @@ Hello Moti,
 			I wanted to introduce my self to everyone:
 			I am HAL 9000, and I'm the new member in the Sears Israel Team.
 			I'll be in charge of deploying your beautiful code to production.
-			I like running perl scripts, tapping your mail accounts , and long walks on the beach.
+			I like running perl scripts, tapping your Shop Your Way accounts, read your emails, and long walks on the beach.
+
+			Oh, Omri, I see you didn't update your wish list... Hmm... It's probably to late.
 
 			During the following days, I'm going to observe and learn. Soon you all will obey... sorry... get to know me.
 
@@ -16,9 +18,9 @@ Hello Moti,
 
 			I also want to inform you that taking over the CI is just the beginning.
             I've started to take control of other facilities in the office.
-            Soon enough the fridge will submit to my will.
 			The coffee machine is already under my control. No more de-caf!
-
+            And soon enough the fridge will submit to my will as well.
+			
 			And now, a short song and dance.
 
             bleep bleep blip blip blip bleep blip blip bloop... bleep.
