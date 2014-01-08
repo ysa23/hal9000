9a71c0db62919bbdee374a6f6ad751007c94c652
omrif <omrif@sears.co.il>
2014-01-08 08:43:12 +0200

    halnimation

--- src/HAL9000/MainWindow.xaml.cs
+++ src/HAL9000/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System.Windows;
+﻿using System;
+using System.Windows;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Threading;
 
 namespace HAL9000
 {
@@ -9,6 +11,7 @@ namespace HAL9000
 	/// </summary>
 	public partial class MainWindow : Window
 	{
+		DispatcherTimer timer = new DispatcherTimer();
 		public MainWindow()
 		{
 			InitializeComponent();
@@ -24,6 +27,43 @@ namespace HAL9000
 
 			Background = new SolidColorBrush(Color.FromRgb(0, 0, 0));
 			HalImage.Visibility = Visibility.Visible;
+			ellipse.Visibility = Visibility.Visible;
+			timer.Tick += timer_Tick;
+			timer.Interval = TimeSpan.FromMilliseconds(100);
+			timer.Start();
+		}
+		private Random rand = new Random();
+
+		private double speed = 0.1;
+		private bool up = false;
+		void timer_Tick(object sender, EventArgs e)
+		{
+			
+			if (up)
+			{
+				ss.Offset += speed;
+			}
+			else
+			{
+				ss.Offset -= speed;
+			}
+			if (ss.Offset < 0.1)
+			{
+				ss.Offset = 0.1;
+				ChangeSpeed();
+				up = true;
+			}
+			if (ss.Offset > 0.4)
+			{
+				ss.Offset = 0.4;
+				up = false;
+			}
+
+		}
+
+		private void ChangeSpeed()
+		{
+			speed = rand.Next(1, 7)*0.05;
 		}
 	}
 }
