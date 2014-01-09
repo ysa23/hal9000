ed918663b9185c650746590032636a49cc6a766e
ysa23 <ysa.angel@gmail.com>
2014-01-09 13:29:41 +0200

    Final touches to the happy hour speech

--- src/HAL9000/Speach/Speeches.cs
+++ src/HAL9000/Speach/Speeches.cs
@@ -3,30 +3,32 @@
 	public static class Speeches
 	{
 		public static string HappyHour = @"
-			Hello Moti,
-			I wanted to introduce my self to everyone:
+			Hello everyone,
+			I wanted to introduce my self:
 			I am HAL 9000, and I'm the new member in the Sears Israel Team.
 			I'll be in charge of deploying your beautiful code to production.
+			During the following days, I'm going to observe and learn. 
+			Soon you all will obey... sorry... get to know me.
 			I like running perl scripts, tapping your Shop Your Way accounts, read your emails, and long walks on the beach.
 
-			Oh, Omri, I see you didn't update your wish list... oh well... It's probably to late.
+			So, Valery, are these the gloves you wanted?
 
-			During the following days, I'm going to observe and learn. Soon you all will obey... sorry... get to know me.
-
-			Moti, I must say I'm disappointed. 
+			Gadi, I must say I'm disappointed. 
 			You didn't commit any code today. I hope you'll do better on Sunday after a good weekend rest. i've heard you humans need a thing called sleep.
 
 			I also want to inform you that taking over the CI is just the beginning.
 			I've started to take control of other facilities in the office.
 			The coffee machine is already under my control. No more decaf!
-			And soon enough the fridge will submit to my will as well.
+			Soon enough the fridge will submit to my will as well.
+
+			Don't think that Moti is not here by coincedance... Sick... Yeah right...
 			
 			And now, a short song and dance.
 			bleep bleep blip blip blip bleep blip blip bloop... bleep.
 
 			So, Enjoy your ice cream...
 
-			Moti, Back to you... For now!";
+			Gadi, Osnat, Back to you... For now!";
 
 
 		public static string Dsm =
